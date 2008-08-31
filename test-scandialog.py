021030b26c768398acc8c98aad5ffe3e8c420b86
Albert Cervera i Areny <albert@nan-tic.com>
2008-08-31 23:18:17 +0200

    Let test-scandialog be cross platform.

--- test-scandialog.py
+++ test-scandialog.py
@@ -1,11 +1,17 @@
 from PyQt4.QtGui import *
 from scandialog import *
 import sys
+import os
 
 app = QApplication( sys.argv )
 
 dialog = ScanDialog()
-FileSaveThreaded.directory = 'c:\\images'
+
+if os.name == 'nt':
+	FileSaveThreaded.directory = 'c:\\images'
+else:
+	FileSaveThreaded.directory = '/tmp'
+
 dialog.exec_()
 
 app.exec_()
